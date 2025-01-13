9ec08fc439c12474b5d5bcf1e00aad1ec9ac9b8d
Dmitry Apraksin <92381646+adw2ru@users.noreply.github.com>
2025-01-14 00:37:54 +0300

    Add files via upload

--- 1_compile_std.bat
+++ 1_compile_std.bat
@@ -4,13 +4,13 @@ echo.
 echo ===========================================================================
 echo Compiling MAIN
 echo ===========================================================================
-..\php\php -f ../scripts/preprocess.php bmain.mac
+..\php\php -f ../scripts/preprocess.php bk3da.mac
 if %ERRORLEVEL% NEQ 0 ( exit /b )
-..\scripts\macro11 -ysl 32 -yus -l _bmain.lst _bmain.mac
+..\scripts\macro11 -ysl 32 -yus -l _bk3da.lst _bk3da.mac
 if %ERRORLEVEL% NEQ 0 ( exit /b )
-..\php\php -f ../scripts/lst2bin.php _bmain.lst ./release/bmain.sav sav
+..\php\php -f ../scripts/lst2bin.php _bk3da.lst ./release/bk3da.sav sav
 
-..\scripts\rt11dsk.exe d main.dsk .\release\bmain.sav >NUL
-..\scripts\rt11dsk.exe a main.dsk .\release\bmain.sav >NUL
+..\scripts\rt11dsk.exe d main.dsk .\release\bk3da.sav >NUL
+..\scripts\rt11dsk.exe a main.dsk .\release\bk3da.sav >NUL
 
 echo.
